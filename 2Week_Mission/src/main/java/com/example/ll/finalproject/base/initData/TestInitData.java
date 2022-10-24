da735fa311f800a06f000eca450968f48d8f2882
sh970901 <aaddss639@naver.com>
2022-10-24 10:10:05 +0900

    2주차 미션, 레코드 폴더 생성

---
+++ 2Week_Mission/src/main/java/com/example/ll/finalproject/base/initData/TestInitData.java
@@ -0,0 +1,21 @@
+package com.example.ll.finalproject.base.initData;
+
+import com.example.ll.finalproject.article.service.ArticleService;
+import com.example.ll.finalproject.keyword.servcice.KeywordService;
+import com.example.ll.finalproject.member.servie.MemberService;
+import com.example.ll.finalproject.product.service.ProductService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestInitData implements InitDataBefore{
+    @Bean
+    CommandLineRunner initData(MemberService memberService, ArticleService articleService, ProductService productService, KeywordService keywordService) {
+        return args -> {
+            before(memberService, articleService, productService, keywordService);
+        };
+    }
+}
