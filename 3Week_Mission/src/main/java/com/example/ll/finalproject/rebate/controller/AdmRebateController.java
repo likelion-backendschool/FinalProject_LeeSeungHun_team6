c62a1a72adb30451a765832889a8ef862d2e7340
sh970901 <aaddss639@naver.com>
2022-10-31 16:45:58 +0900

    건별데이터 정산

--- 3Week_Mission/src/main/java/com/example/ll/finalproject/rebate/controller/AdmRebateController.java
+++ 3Week_Mission/src/main/java/com/example/ll/finalproject/rebate/controller/AdmRebateController.java
@@ -3,14 +3,14 @@ package com.example.ll.finalproject.rebate.controller;
 import com.example.ll.finalproject.base.dto.RsData;
 import com.example.ll.finalproject.rebate.entity.RebateOrderItem;
 import com.example.ll.finalproject.rebate.service.RebateService;
+import com.example.ll.finalproject.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -53,6 +53,20 @@ public class AdmRebateController {
         return "adm/rebate/rebateOrderItemList";
     }
 
+    @PostMapping("/rebateOne/{orderItemId}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String rebateOne(@PathVariable long orderItemId, HttpServletRequest req) {
+        RsData rebateRsData = rebateService.rebate(orderItemId);
 
+        String referer = req.getHeader("Referer");
+
+        String yearMonth = Ut.url.getQueryParamValue(referer, "yearMonth", "");
+
+        String redirect = "redirect:/adm/rebate/rebateOrderItemList?yearMonth=" + yearMonth;
+
+        redirect = rebateRsData.addMsgToUrl(redirect);
+
+        return redirect;
+    }
 
 }
--- 3Week_Mission/src/main/java/com/example/ll/finalproject/rebate/service/RebateService.java
+++ 3Week_Mission/src/main/java/com/example/ll/finalproject/rebate/service/RebateService.java
@@ -2,6 +2,8 @@ package com.example.ll.finalproject.rebate.service;
 
 
 import com.example.ll.finalproject.base.dto.RsData;
+import com.example.ll.finalproject.cash.entity.CashLog;
+import com.example.ll.finalproject.member.servie.MemberService;
 import com.example.ll.finalproject.order.entity.OrderItem;
 import com.example.ll.finalproject.order.service.OrderService;
 import com.example.ll.finalproject.rebate.entity.RebateOrderItem;
@@ -20,6 +22,8 @@ import java.util.stream.Collectors;
 public class RebateService {
     private final OrderService orderService;
     private final RebateOrderItemRepository rebateOrderItemRepository;
+
+    private final MemberService memberService;
     @Transactional
     public RsData makeDate(String yearMonth) {
         int monthEndDay = Ut.date.getEndDayOf(yearMonth);
@@ -67,5 +71,32 @@ public class RebateService {
         LocalDateTime toDate = Ut.date.parse(toDateStr);
 
         return rebateOrderItemRepository.findAllByPayDateBetweenOrderByIdAsc(fromDate, toDate);
+    }
+
+    @Transactional
+    public RsData rebate(long orderItemId) {
+        RebateOrderItem rebateOrderItem = rebateOrderItemRepository.findByOrderItemId(orderItemId).get();
+
+        if (rebateOrderItem.isRebateAvailable() == false) {
+            return RsData.of("F-1", "정산을 할 수 없는 상태입니다.");
+        }
+
+        int calculateRebatePrice = rebateOrderItem.calculateRebatePrice();
+
+        CashLog cashLog = memberService.addCash(
+                rebateOrderItem.getProduct().getAuthor(),
+                calculateRebatePrice,
+                "정산__%d__지급__예치금".formatted(rebateOrderItem.getOrderItem().getId())
+        ).getData().getCashLog();
+
+        rebateOrderItem.setRebateDone(cashLog.getId());
+
+        return RsData.of(
+                "S-1",
+                "주문품목번호 %d번에 대해서 판매자에게 %s원 정산을 완료하였습니다.".formatted(rebateOrderItem.getOrderItem().getId(), calculateRebatePrice),
+                Ut.mapOf(
+                        "cashLogId", cashLog.getId()
+                )
+        );
     }
 }
